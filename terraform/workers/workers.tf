ca012d200cf10b7b46928bf8230dbacf89a6479b
kubealex <al.rossi87@gmail.com>
2022-01-23 14:12:24 +0100

    Bumped provider version

--- terraform/workers/workers.tf
+++ terraform/workers/workers.tf
@@ -93,11 +93,11 @@ resource "libvirt_domain" "worker" {
 }
 
 terraform {
- required_version = ">= 0.13"
+ required_version = ">= 1.0"
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.6.11"
+      version = "0.6.12"
     }
   }
 }
